657364eabdbc51acc37cc878eab48613345e0516
Stevespear426 <stephen.k.spear@gmail.com>
2015-12-28 19:04:27 +0100

    FWBcmte: More system animations improvements (1/2)
    Brought back from kitkat by @LorDClockaN
    
    Change-Id: I7974c117c46adb8210d357434912a2cf15fe3204

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4785,6 +4785,12 @@ public final class Settings {
         public static final String STATUS_BAR_NETWORK_TRAFFIC_ICON_COLOR = "status_bar_network_traffic_icon_color";
 
 
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        public static final String ANIMATION_CONTROLS_EXIT_ONLY = "animation_controls_exit_only";
+
+        public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
+
         /**
          * Whether to apply a shadow on top of the header image
          * value is the alpha value of the shadow image is 0 -> no shadow -> 255 black

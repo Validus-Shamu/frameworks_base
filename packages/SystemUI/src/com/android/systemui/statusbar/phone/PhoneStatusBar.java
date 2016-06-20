97fca8a2f7f9aa996bdfffe20cfd7b8265410af8
Corey Edwards <ensabahnur16@gmail.com>
2016-06-20 08:55:10 -0500

    Fix custom lockscreen after adding visualizer
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2147,6 +2147,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (wm != null) {
             BitmapDrawable wallpaperDrawable = (BitmapDrawable) wm.getDrawable();
             if (wallpaperDrawable != null) {
+				backdropBitmap = wm.getKeyguardBitmap();
                 wallpaperBitmap = wallpaperDrawable.getBitmap();
             }
         }
